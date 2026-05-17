0cd2f97958374f14f849be08ed4ab68e1229cf14
Hristo Alexiev <59394384+halexiev-softcivilization@users.noreply.github.com>
2026-05-18 01:44:52 +0300

    chore: upgrade to Java 25 (fully supported by Spring Boot 4.0)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "com.xlinks"
 version = "0.1.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_21
+java.sourceCompatibility = JavaVersion.VERSION_25
 
 repositories {
     mavenCentral()
@@ -47,7 +47,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+        jvmTarget = "25"
     }
 }
 
